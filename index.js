9c3cc01c01bd6d30cc6cac366b62091dabbd7a00
Akhator Itua <iakhator@gmail.com>
2020-09-02 18:10:31 +0100

    feat: add documentation and routes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.use(morgan('common'));
+app.use(bodyParser.json())
 app.use(express.static('public'));
 
 let movies = [
@@ -18,10 +20,87 @@ let movies = [
   {"title":"New Life Rescue","year":1900,"cast":[],"genres":[]}
 ]
 
-// GET requests
+// GET all movies
 app.get('/movies', (req, res) => {
-  res.send(movies);
+  res.json(movies)
 });
+
+// get single movies based on title
+app.get('/movies/:title', (req, res) => {
+  res.json({
+    title:"Feeding Sea Lions",
+    year:1900,
+    cast:["Paul Boyton"],
+    genres:[]
+  });
+});
+
+//Get movie genre based on titles or name
+app.get('/movies/genre/:title', (req, res) => {
+  res.json({
+    genre: 'Action'
+  });
+});
+
+//Get a movie director based on name
+app.get('/movies/director/:name', (req, res) => {
+  res.json({
+    name: 'Russo Alex',
+    birth_year: '2019',
+    death_year: 'nil'
+  });
+});
+
+//Register users
+app.post('/users', (req, res) => {
+  console.log(req.body)
+  const userDetails = {
+    username: req.body.username,
+    email: req.body.email,
+    id: 1,
+  }
+
+  res.json({userDetails})
+})
+
+//update users
+app.put('/users/:id', (req, res) => {
+  const userName = req.body.username;
+  res.json({message: 'Updated successfully'})
+})
+
+//users add favourite movie
+app.post('/users/:id/favourites', (req, res) => {
+  res.json({
+    message: "Movies successfully added to favourite"
+  })
+})
+
+//delete favourite movies
+app.delete('/users/:id/favourites/:favourite_id', (req, res) => {
+  res.json({
+    message: "Favourite movie deleted successfully"
+  })
+})
+
+//delete user
+app.delete('/users/:id', (req, res) => {
+  res.json({
+    message: "User deleted successfully"
+  })
+})
+
+
+    // Return a list of ALL movies to the user
+    // Return data (description, genre, director, image URL, whether it’s featured or not) about a single movie by title to the user
+    // Return data about a genre (description) by name/title (e.g., “Thriller”)
+    // Return data about a director (bio, birth year, death year) by name
+    // Allow new users to register
+    // Allow users to update their user info (username)
+    // Allow users to add a movie to their list of favorites (showing only a text that a movie has been added—more on this later)
+    // Allow users to remove a movie from their list of favorites (showing only a text that a movie has been removed—more on this later)
+    // Allow existing users to deregister (showing only a text that a user email has been removed—more on this later)
+
 
 
 app.use((err, req, res, next) => {
